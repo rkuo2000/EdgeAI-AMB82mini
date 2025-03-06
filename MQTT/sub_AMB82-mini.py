a48f928ca2bbdf8b1c51f1af342390e5e8e68b0a
Richard Kuo <rkuo2000@gmail.com>
2025-03-06 20:50:05 +0800

    Delete MQTT/sub_AMB82-mini.py

--- MQTT/sub_AMB82-mini.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.subscribe as subscribe
-
-host = "test.mosquitto.org"
-
-msg = subscribe.simple("ntou/edgeai/robot1", hostname=host)
-print("topic  : %s" % (msg.topic))
-print("payload: %s" % (msg.payload.decode("utf-8")))
-print()
