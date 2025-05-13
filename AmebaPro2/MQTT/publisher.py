4571cf2521c5206beb1cf7b9fa37fd6bd2bb086a
Richard Kuo <rkuo2000@gmail.com>
2025-05-14 00:58:24 +0800

    Update publisher.py

--- AmebaPro2/MQTT/publisher.py
+++ AmebaPro2/MQTT/publisher.py
@@ -4,11 +4,11 @@ import paho.mqtt.publish as publish
 host = "mqtt.eclipseprojects.io"
 
 # single
-publish.single("paho/ntou/edgeai/robot1", "come to the living room", hostname=host)
+publish.single("ntou/edgeai/robot1", "come to the living room", hostname=host)
 
-publish.single("paho/ntou/edgeai/robot2", "go to the kitchen", hostname=host)
+publish.single("ntou/edgeai/robot2", "go to the kitchen", hostname=host)
 
 ## multiple
-msgs = [{'topic':"paho/ntou/edgeai/robot1", 'payload':"At Smarter"}, 
-    ("paho/ntou/edgeai/robot2", "RoboDog", 0, False)]
+msgs = [{'topic':"ntou/edgeai/robot1", 'payload':"At Smarter"}, 
+    ("ntou/edgeai/robot2", "RoboDog", 0, False)]
 publish.multiple(msgs, hostname=host, protocol=MQTTProtocolVersion.MQTTv5)
