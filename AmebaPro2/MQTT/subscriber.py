e107bd926c3621924544d1a37897efd8df8d5408
Richard Kuo <rkuo2000@gmail.com>
2025-05-14 00:58:57 +0800

    Update subscriber.py

--- AmebaPro2/MQTT/subscriber.py
+++ AmebaPro2/MQTT/subscriber.py
@@ -1,11 +1,7 @@
 import paho.mqtt.subscribe as subscribe
 
 host = "mqtt.eclipseprojects.io"
-msg = subscribe.simple("paho/ntou/edgeai/robot1", hostname=host)
+msg = subscribe.simple("ntou/edgeai/robot1", hostname=host)
 print("topic  : %s" % (msg.topic))
 print("payload: %s" % (msg.payload.decode("utf-8")))
 print()
-
-msg = subscribe.simple("paho/ntou/edgeai/robot1", hostname=host)
-print("topic  : %s" % (msg.topic))
-print("payload: %s" % (msg.payload.decode("utf-8")))
