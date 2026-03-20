b81a5b4d8c542b495e1e5f17ce3ee7e45133f40a
Richard Kuo <rkuo2000@gmail.com>
2026-03-20 18:22:46 +0800

    Add files via upload

---
+++ Arduino/AMB82-mini/BasicNTP_OTA/BasicNTP_OTA.ino
@@ -0,0 +1,44 @@
+/*
+ Example guide:
+ https://ameba-doc-arduino-sdk.readthedocs-hosted.com/en/latest/ameba_pro2/amb82-mini/Example_Guides/NTP/Retrieve%20Universal%20Time%20%28UTC%29%20by%20NTPClient%20library.html
+*/
+#include "OTA.h"
+#include <NTPClient.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+char ssid[] = "HITRON-DF90-2.4G";
+char pass[] = "0972211921";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+int port = 3000;
+char* server = "192.168.0.22";
+
+OTA ota;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    timeClient.begin();
+    // Set up the threads
+    ota.start_OTA_threads(port, server);
+}
+
+void loop()
+{
+    timeClient.update();
+
+    Serial.println(timeClient.getFormattedTime());
+
+    delay(1000);
+}
